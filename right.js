d209890058859a41d1c4fb76c73016cefc865672
Nyarko28 <161228198+Nyarko28@users.noreply.github.com>
2024-07-12 18:54:33 +0100

    Add files via upload

---
+++ right.js
@@ -0,0 +1,13 @@
+window.addEventListener('load',()=>{
+    for (let i = 0; i <=20 ; i++) {
+        
+        
+    
+  document.querySelector('head').innerHTML+=`<!-- this is the work of emmanuel nyarko 0534086538 
+     emmanuelnyarko284@gmail.com
+     -->`;
+     console.log(`<!-- this is the work of emmanuel nyarko 0534086538 
+     emmanuelnyarko284@gmail.com
+     -->`)
+    }
+})
